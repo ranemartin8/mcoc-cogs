f7193044ab9fd01564e0e1edf5faa96d3dd2c330
Rane Martin <ranemartin8@gmail.com>
2017-07-13 13:30:46 -0400

    a

--- gsheeter/gsheeter.py
+++ gsheeter/gsheeter.py
@@ -89,24 +89,45 @@ class MemberFinder(commands.Converter):
 				checkfor_str = key.find(user_string)
 				if checkfor_str != -1:
 					results.append(str(value))
-#			if results: 
-#				print('Result(s): '+', '.join(results)
-			#if there are 5 or more possible results
-			if len(results) > 4:
-				results_count = len(results)
-				firstfour = ', '.join(results[0:5])
-				user = 'error'
-				await self.ctx.bot.say("Too many possible matches found ({} in total): {}. Please be more specific and try again.".format(results_count,firstfour+'...'))
-			elif results:
-				firstresult = results[0]
-				user = server.get_member(firstresult)
-				find_method = 'User found by partial string matching'
-				if len(results) > 1: 
+			if results:
+				#TOO MANY: if there are 5 or more possible results
+				if len(results) > 4: 
+					results_count = len(results)
 					for mem_id in results:
 						ser_mem = server.get_member(mem_id)
 						result_names.append(ser_mem.display_name)
-					await self.ctx.bot.say("Multiple possible matches found: {}\n\n"
-										   "So, I just went with first match: **{}**".format(', '.join(result_names),user.display_name))	
+					firstfour = ', '.join(result_names[0:5])
+					user = 'error'
+					await self.ctx.bot.say("Too many possible matches found ({} in total): {}\n\nPlease be more specific and try again.".format(results_count,firstfour+'...'))
+				#Less than 4 results
+				else:
+					firstresult = results[0]
+					user = server.get_member(firstresult)
+					find_method = 'User found by partial string matching'
+					if len(results) > 1: 
+						for mem_id in results:
+							ser_mem = server.get_member(mem_id)
+							result_names.append(ser_mem.display_name)
+						await self.ctx.bot.say("Multiple possible matches found: {}\n\n"
+											   "So, I just went with first match: **{}**".format(', '.join(result_names),user.display_name))
+#			if len(results) > 4:
+#				results_count = len(results)
+#				for mem_id in results:
+#						ser_mem = server.get_member(mem_id)
+#				firstfour = ', '.join(results[0:5])
+#				user = 'error'
+#				await self.ctx.bot.say("Too many possible matches found ({} in total): {}. Please be more specific and try again.".format(results_count,firstfour+'...'))
+#			elif results:
+#				
+#				firstresult = results[0]
+#				user = server.get_member(firstresult)
+#				find_method = 'User found by partial string matching'
+#				if len(results) > 1: 
+#					for mem_id in results:
+#						ser_mem = server.get_member(mem_id)
+#						result_names.append(ser_mem.display_name)
+#					await self.ctx.bot.say("Multiple possible matches found: {}\n\n"
+#										   "So, I just went with first match: **{}**".format(', '.join(result_names),user.display_name))	
 			else:
 				matches = difflib.get_close_matches(user_string,mem_dict.keys(), n=3, cutoff=0.5)
 				if matches:
