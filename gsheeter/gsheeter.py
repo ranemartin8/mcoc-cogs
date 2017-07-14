8ea90717e286a6781a73b93ea5531d575662fe6e
Rane Martin <ranemartin8@gmail.com>
2017-07-13 22:15:40 -0400

    [0]

--- gsheeter/gsheeter.py
+++ gsheeter/gsheeter.py
@@ -479,15 +479,15 @@ class gsheet_cog:
 			em.add_field(name='**'+memInfo['name']+'**',value='\n'+status+'\n'+joined_on+'\n\n'
 						 'Battlegroup: **'+memInfo['bg']+'**\n'
 						 'Local Time: **'+memInfo['localtime']+'**  '+memInfo['clockemoji'],inline=False)
-			if memInfo['a_team']:
+			if memInfo['a_team'][0]:
 				em.add_field(name='**A-Team**',value='\n'.join(memInfo['a_team']))
-			if memInfo['b_team']:
+			if memInfo['b_team'][0]:
 				em.add_field(name='**B-Team**',value='\n'.join(memInfo['b_team']))
-			if memInfo['defense']:
+			if memInfo['defense'][0]:
 				em.add_field(name='**AW Defense**',value='\n'.join(memInfo['defense']))
 			if memInfo['paths']:
 				em.add_field(name='**Paths**',value=memInfo['paths'],inline=False)
-				# and memInfo['paths'] != 'No paths found'
+		print(em.to_dict())
 		try:
 			await self.bot.say(embed=em)
 			await self.bot.delete_message(search_msg)
