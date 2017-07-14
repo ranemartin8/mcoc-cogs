2a6bda9e21558321c5f32cf38a37fb03250d0af3
Rane Martin <ranemartin8@gmail.com>
2017-07-13 22:16:51 -0400

    0:3

--- gsheeter/gsheeter.py
+++ gsheeter/gsheeter.py
@@ -479,11 +479,11 @@ class gsheet_cog:
 			em.add_field(name='**'+memInfo['name']+'**',value='\n'+status+'\n'+joined_on+'\n\n'
 						 'Battlegroup: **'+memInfo['bg']+'**\n'
 						 'Local Time: **'+memInfo['localtime']+'**  '+memInfo['clockemoji'],inline=False)
-			if memInfo['a_team'][0]:
+			if memInfo['a_team'][0:3]:
 				em.add_field(name='**A-Team**',value='\n'.join(memInfo['a_team']))
-			if memInfo['b_team'][0]:
+			if memInfo['b_team'][0:3]:
 				em.add_field(name='**B-Team**',value='\n'.join(memInfo['b_team']))
-			if memInfo['defense'][0]:
+			if memInfo['defense'][0:3]:
 				em.add_field(name='**AW Defense**',value='\n'.join(memInfo['defense']))
 			if memInfo['paths']:
 				em.add_field(name='**Paths**',value=memInfo['paths'],inline=False)
