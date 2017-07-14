80ed18752e693b7dbc46643ca638127b1c268471
Rane Martin <ranemartin8@gmail.com>
2017-07-13 21:52:37 -0400

    defense

--- gsheeter/gsheeter.py
+++ gsheeter/gsheeter.py
@@ -476,7 +476,7 @@ class gsheet_cog:
 				em.add_field(name='**A-Team**',value='\n'.join(memInfo['a_team']))
 			if memInfo['b_team'][0]:
 				em.add_field(name='**B-Team**',value='\n'.join(memInfo['b_team']))
-			if memInfo['aw'][0]:
+			if memInfo['defense'][0]:
 				em.add_field(name='**AW Defense**',value='\n'.join(memInfo['defense']))
 			if memInfo['paths']:
 				em.add_field(name='**Paths**',value=memInfo['paths'],inline=False)
