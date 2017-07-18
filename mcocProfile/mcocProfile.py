823daa0109268def043429738bad378ed23673d8
Rane Martin <ranemartin8@gmail.com>
2017-07-18 16:06:39 -0400

    args

--- mcocProfile/mcocProfile.py
+++ mcocProfile/mcocProfile.py
@@ -153,11 +153,12 @@ class mcocProfile:
 				await self.bot.say('No **{}** available to delete.'.format(field_name))
 				
 	@mcoc_profile.command(pass_context=True)
-	async def display(self, ctx, *, field: str, setting : str):
+	async def display(self, ctx, field: str, setting : str):
 		"""
 		Toggle the visibility of a field on your profile."""	
 		author = ctx.message.author
 		user_id = author.id
+		
 		toggles = {'show','hide'}
 		toggle.lower()
 		field.lower()
